147799c47337340b2dfbcd5bb324e66010b5c640
Igor <traver.unimmer@gmail.com>
2023-01-16 16:30:35 +0300

    feat: complete task 02 backend-it-kamasutra

--- 02/server.js
+++ 02/server.js
@@ -1,15 +1,46 @@
 const http = require('http');
+const fs = require('fs');
 
-const app = http.createServer((req,res) => {
+const delay = (ms) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, ms)
+    });
+};
+
+const readFile = (path) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, (err, data) => {
+            if (err) reject(err)
+            else resolve(data);
+        })
+    })
+}
+
+const app = http.createServer(async (req, res) => {
+    let data = '404 not found';
     switch (req.url) {
         case '/home': {
-            const data = 'best free online course';
-            res.write(data);
-            res.end();
+            try {
+                const data = await readFile('pages/home.html');
+                res.write(data);
+                res.end();
+            } catch (e) {
+                res.write('Something wrong');
+                res.end();
+            }
+            break;
+        }
+        case '/about': {
+            await delay(3000);
+            res.write('ABOUT COURSE');
+            res.end()
+            break;
         }
         default: {
-            res.write('404 not found');
             res.end();
+            break;
         }
     }
 })
